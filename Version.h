04fac67a75ad11212c32e32796b66a839c03608f
Sun_Dancer78 <42901216+Sundancer78@users.noreply.github.com>
2020-03-07 13:45:10 +0100

    Update Version.h

--- Version.h
+++ Version.h
@@ -34,19 +34,19 @@
  * Verbose version identifier which should contain a reference to the location
  * from where the binary was downloaded or the source code was compiled.
  */
-#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (01)"
+#define DETAILED_BUILD_VERSION SHORT_BUILD_VERSION " (02)"
 
 /**
  * The STRING_DISTRIBUTION_DATE represents when the binary file was built,
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2020-02-29"
+#define STRING_DISTRIBUTION_DATE "2020-03-07"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "SKR-1.4T Ender3"
+#define MACHINE_NAME "SKR-1.4Turbo Ender3"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
@@ -54,7 +54,7 @@
  * has a distinct Github fork— the Source Code URL should just be the main
  * Marlin repository.
  */
-//#define SOURCE_CODE_URL "https://github.com/MarlinFirmware/Marlin"
+#define SOURCE_CODE_URL "https://github.com/Sundancer78/Marlin-2.0.4_SKR_14_turbo_ender3"
 
 /**
  * Default generic printer UUID.
